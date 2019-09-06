462c4005ac7783d2faf70c2b89edeaaf2eebfdda
Hans <hans@qbil.nl>
2019-09-06 13:01:49 +0200

    Result from the kata and a bit of time at home

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -4,5 +4,104 @@ namespace Rover;
 
 final class MarsRover
 {
+    const x = 0;
+    const y = 1;
+    const GRID_SIZE = 256;
+    private $position;
+    private $direction;
+    private $obstacles;
+    private $stopped;
+    const directions = [[1, self::y], [1, self::x], [-1, self::y], [-1, self::x]];
+    const compassBearings = ['N' => 0, 'E' => 1, 'S' => 2, 'W' => 3];
 
+    /**
+     * MarsRover constructor.
+     * @param int $x
+     * @param int $y
+     * @param int $bearing
+     * @throws \Exception
+     */
+    public function __construct(int $x, int $y, string $bearing)
+    {
+        if ($x < 0 || $x >= self::GRID_SIZE || $y < 0 || $y >= self::GRID_SIZE) {
+            throw new \Exception('Illegal position');
+        }
+        $this->position = [self::x => $x, self::y => $y];
+        if (!array_key_exists($bearing, self::compassBearings)) {
+            throw new \Exception('Non-existent bearing');
+        }
+        $this->direction = self::compassBearings[$bearing];
+        $this->obstacles = [];
+        $this->stopped = false;
+    }
+
+    public function move($commands)
+    {
+        foreach ($commands as $command) {
+            $this->handleCommand($command);
+            if ($this->stopped) break;
+        }
+    }
+
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    private function handleCommand($command)
+    {
+        if ($command == 'F') {
+            $this->moveSteps(1);
+        }
+        if ($command == 'B') {
+            $this->moveSteps(-1);
+        }
+        if ($command == 'R') {
+            $this->turnRight();
+        }
+        if ($command == 'L') {
+            $this->turnLeft();
+        }
+    }
+
+    private function turnLeft()
+    {
+        $this->turnRight();
+        $this->turnRight();
+        $this->turnRight();
+    }
+
+    private function turnRight()
+    {
+        $this->direction = ($this->direction + 1) % 4;
+    }
+
+    private function moveSteps($steps)
+    {
+        list($direction, $axis) = self::directions[$this->direction];
+        $this->position[$axis] += $steps * $direction;
+        $this->wrapAround();
+
+        $this->stopAtObstacle($steps);
+    }
+
+    public function setObstacles(array $obstacles)
+    {
+        $this->obstacles = $obstacles;
+    }
+
+    private function wrapAround()
+    {
+        $this->position = array_map(function ($coordinate) {
+            return ($coordinate + self::GRID_SIZE) % self::GRID_SIZE;
+        }, $this->position);
+    }
+
+    private function stopAtObstacle($steps)
+    {
+        if (in_array($this->position, $this->obstacles)) {
+            $this->moveSteps($steps * -1);
+            $this->stopped = true;
+        }
+    }
 }
--- tests/MarsRoverTest.php
+++ tests/MarsRoverTest.php
@@ -7,12 +7,84 @@ use Rover\MarsRover;
 
 final class MarsRoverTest extends TestCase
 {
-    public function test_that_it_does_not_work()
+    private $rover;
+
+    public function setUp()
     {
-        $rover = new MarsRover();
+        $this->rover = new MarsRover(0, 0, 'N');
+    }
 
-        // something...
+    public function test_that_it_starts_at_zero_zero()
+    {
+        $this->rover->move([]);
+        self::assertEquals([0, 0], $this->rover->getPosition());
+    }
 
-        self::assertTrue(false);
+    public function test_that_it_moves_from_initial_position()
+    {
+        $this->rover->move(['F']);
+        self::assertEquals([0, 1], $this->rover->getPosition());
+    }
+
+    public function test_that_it_moves_backwards()
+    {
+        $this->rover->move(['F', 'B']);
+        self::assertEquals([0, 0], $this->rover->getPosition());
+    }
+
+    public function test_that_it_moves_right()
+    {
+        $this->rover->move(['R']);
+        self::assertEquals([0, 0], $this->rover->getPosition());
+    }
+
+    public function test_that_it_moves_forward_and_right()
+    {
+        $this->rover->move(['F', 'R', 'F']);
+        self::assertEquals([1, 1], $this->rover->getPosition());
+    }
+
+    public function test_that_it_moves_forward_and_right_and_left_and_right()
+    {
+        $this->rover->move(['F', 'R', 'F', 'R', 'F']);
+        self::assertEquals([1, 0], $this->rover->getPosition());
+    }
+
+    public function test_that_it_moves_in_any_direction()
+    {
+        $this->rover->move(['F', 'R', 'R', 'L', 'F', 'R', 'B', 'B']);
+        self::assertEquals([1, 3], $this->rover->getPosition());
+    }
+
+    public function test_that_grid_wraps_around()
+    {
+        $this->rover->move(['B']);
+        self::assertEquals([0, 255], $this->rover->getPosition());
+    }
+
+    public function test_that_it_stops_at_obstacles()
+    {
+        $this->rover->setObstacles([[2,2]]);
+        $this->rover->move(['F','F','R','F','F']);
+        self::assertEquals([1,2], $this->rover->getPosition());
+    }
+
+    public function test_that_it_stops_after_meeting_first_obstacle()
+    {
+        $this->rover->setObstacles([[2,2]]);
+        $this->rover->move(['F','F','R','F','F','B']);
+        self::assertEquals([1,2], $this->rover->getPosition());
+    }
+
+    public function test_that_it_only_accepts_existing_direction()
+    {
+        self::expectException(\Exception::class);
+        $rover = new MarsRover(0, 0, 'X');
+    }
+
+    public function test_that_it_only_accepts_valid_coordinates()
+    {
+        self::expectException(\Exception::class);
+        $rover = new MarsRover(-1, 0, 'N');
     }
 }
